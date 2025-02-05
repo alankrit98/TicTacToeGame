f2de76b0891d5022901350c684773f9906c9e4c2
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-02-05 17:57:02 +0530

    Complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './Components/TicTacToe.jsx/TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
